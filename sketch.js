5a7389dc196e67fd8ddc602ca0ec5753a5360738
ramadugusatish <88569231+ramadugusatish@users.noreply.github.com>
2021-08-14 09:46:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyIsDown(DOWN_ARROW))
+{
+  background("red");
+}
+if(keyIsDown(UP_ARROW))
+{
+  background("YELLOW");
+}
+  if(keyIsDown(RIGHT_ARROW))
+{
+  background("DARKBLUE");
+}
+  
+if(keyIsDown(LEFT_ARROW))
+{
+  background("ORANGE");
+}
+drawSprite();
+}
